419419570286d9343ba44b5d222e78d5d77f7a26
manishnaik69 <121090600+manishnaik69@users.noreply.github.com>
2025-03-13 05:53:47 -0400

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World PES1UG22CS330\n";
+  cout << "Hello, Jenkins PES1UG22CS330\n";
+  cout << "PES1UG22CS330 has successfully built and run this  \n";
+  return 0;
+}
